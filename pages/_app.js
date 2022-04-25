571b460ed03ab7a5ff2ab46071d1fb29284ec6b7
ambx <hibaf.me18@sahyadri.edu.in>
2022-04-25 17:36:17 +0530

    fixed font abnormalities

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '../components/Layout/index'
 import '../styles/global.scss'
+import "@fontsource/montserrat"
+import "@fontsource/poppins"
 
 export default function MyApp({ Component, pageProps }) {
   return (
